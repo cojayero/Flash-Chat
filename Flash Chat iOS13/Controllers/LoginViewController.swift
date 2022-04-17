a94ccc4bcbdfb7cf56dba75c274d80c5ae935d2b
Cojayero <51817445+cojayero@users.noreply.github.com>
2022-04-17 09:33:11 +0200

    q

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
@@ -15,6 +15,12 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        if  let email = emailTextfield.text, let password = passwordTextfield.text {
+
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+            guard let strongSelf = self else {return}
+        }
+        }
     }
     
 }
